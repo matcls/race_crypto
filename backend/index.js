543b02a909b8ba1525a65c5f2b315ff30cdbeadf
Manuel Torres <manueltorresvesga@gmail.com>
2022-04-27 12:36:19 -0500

    Fetch data from API
    Make the requests and get data using axios library

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const express = require("express");
 const socketIO = require("socket.io");
+const axios = require("axios");
 
-const app = express();
-
-const server = app.listen(PORT, () => {
+const server = express().listen(PORT, () => {
   console.log(`Listining to ${PORT}`);
 });
 
@@ -19,5 +18,36 @@ socketHandler.on("connection", (socket) => {
     console.log("Client disconnected!");
   });
   console.log("Client Connected!");
-  socket.emit("Hello RACE Client!");
+  socket.emit("crypto", "Connected to RACE!");
 });
+
+const getPrices = () => {
+  axios
+    .get(process.env.LIST_URL, {
+      headers: {
+        "x-messari-api-key": process.env.API_KEY,
+      },
+    })
+    .then((response) => {
+      const priceList = response.data.data.map((item) => {
+        return {
+          id: item.id,
+          name: item.symbol,
+          price: item.metrics.market_data.price_usd,
+        };
+      });
+
+      socketHandler.emit("crypto", priceList);
+    })
+    .catch((err) => {
+      console.log(err);
+      socketHandler.emit("crypto", {
+        error: true,
+        message: "Error Fetching Prices Data From API",
+      });
+    });
+};
+
+setInterval(() => {
+  getPrices();
+}, 20000);
