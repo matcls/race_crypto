224fe876354fa8e7d67fa24dbea306bd0c697a36
Manuel Torres <manueltorresvesga@gmail.com>
2022-04-28 09:40:23 -0500

    Add some constant variables for colors

--- Race/consts/app-consts.tsx
+++ Race/consts/app-consts.tsx
@@ -1,1 +1,5 @@
 export const API_URL = 'http://192.168.0.25:3000';
+export const GREEN = "#2DB572";
+export const RED = "#EE225D";
+export const LIGHTGREY = "#ADB7C3"
+export const LIGHTBLACK = "#4E5B69"
